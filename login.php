930f7639159a47a86747b69f476ca54e05402001
Fernando Venuto <95356940+FernandoVenuto@users.noreply.github.com>
2022-04-27 17:14:43 -0300

    Recover Password
    Css and Html

--- login.php
+++ login.php
@@ -15,34 +15,37 @@
         <div class="container">
             <div class="row g-0 d-flex justify-content-md-center align-items-center vh-100">
                 <div class="col-7 box text-center">
-                    <div class="col-md-12">
-                        <img class="logo-dmie" src="imagens/data_mining_pb_nome_1400_1000.png" alt="">
-                    </div>
-                    <div class="row g-0">
-                        <div class="col-3 justify-content-md-center">
+                    <form action="">
+                        <div class="col-md-12">
+                            <img class="logo-dmie" src="imagens/data_mining_pb_nome_1400_1000.png" alt="">
                         </div>
-                        <div class="col-6 justify-content-md-center mt-3">
-                            <div class="justify-content-lg-center">
-                                <form action="">
-                                    <input class="col-12 login" type="text" placeholder="Usuário" name="usuario" required >
-                                    <input class="col-12 login" type="password" placeholder="Senha" name="senha" required >
-                                </form>
+                        <div class="row g-0">
+                            <div class="col-3 justify-content-md-center">
+                            </div>
+                            <div class="col-6 justify-content-md-center mt-3">
+                                <div class="justify-content-lg-center">
+
+                                    <input class="col-12 login" type="text" placeholder="Usuário" name="usuario" required>
+                                    <input class="col-12 login" type="password" placeholder="Senha" name="senha" required>
+
+                                </div>
+                            </div>
+                            <div class="col-3 justify-content-md-center">
                             </div>
                         </div>
-                        <div class="col-3 justify-content-md-center">
+                        <div class="text">
+                            <p>
+                                Esqueceu a senha? <strong><a href="recuperar-senha.php">Clique aqui</a></strong>
+                            </p>
                         </div>
-                    </div>
-                    <div class="text">
-                        <p>
-                            Esqueceu a senha? <strong><a href="">Clique aqui</a></strong>
-                        </p>
-                    </div>
-                    <div>
-                        <button id="logar"><span>Login </span></button>
-                    </div>
+                        <div>
+                            <button id="logar"><span>Login </span></button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </main>
 </body>
+
 </html>
