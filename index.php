bb8947d428f98f3b038be1af551a7bbe46cb9eb9
Fernando Venuto <95356940+FernandoVenuto@users.noreply.github.com>
2022-05-02 17:25:22 -0300

    dashboard finished
    Falta arrumar o menu lateral

--- index.php
+++ index.php
@@ -29,12 +29,12 @@
             </div>
         </div>
         <div class="row dashboard">
-            <nav class="pt-3 d-none d-md-block col-md-2 col-lg-2 col- col-xxl-2 dashboard-menu-bg sidebar dashboard">
+            <nav class="pt-3 d-none d-md-block col-md-2 col-lg-2 col- col-xxl-2 dashboard-menu-bg sidebar dashboard" style="height: 79vh">
                 <div class="sidebar-sticky dashboard">
                     <ul class="nav flex-column dashboard">
                         <li class="nav-item dashboard">
                             <a class="nav-link active dashboard" href="#">
-                            <img src="imagens/nav-service.webp" width="24" height="24">
+                                <img src="imagens/nav-service.webp" width="24" height="24">
                                 <span class="nav-mobile">Serviços</span>
                             </a>
                         </li>
@@ -61,19 +61,132 @@
             </nav>
             <!--right-->
             <div class="dashboard  col-md-10 col-lg-10 col-xxl-10 dashboard-bg2">
+                <!--Box 1-->
                 <div class="container dashboard-box mt-3">
                     <div class=" row dashboard">
                         <div class="col-3 d-inline-block">
-                            <h3>Visual Aid</h2>
+                            <h3>Visual Aid</h3>
                         </div>
                         <div class="col-3 d-inline-block">
-                            <h3>Landing Page</h2>
+                            <h3>Landing Page</h3>
                         </div>
                         <div class="col-3 d-inline-block">
-                            <h3>Email</h2>
+                            <h3>Email</h3>
                         </div>
                         <div class="col-3 d-inline-block">
-                            <h3>WhatsApp</h2>
+                            <h3>WhatsApp</h3>
+                        </div>
+                    </div>
+                </div>
+                <!--Box 2-->
+                <div class="container dashboard-box2 mt-3 d-flex">
+                    <div class="col-12 row dashboard align-items-end">
+                        <div class="col-1">
+                            <h4>Janeiro</h4>
+                        </div>
+                        <div class="col-1">
+                            <h4>Fevereiro</h4>
+                        </div>
+                        <div class="col-1">
+                            <h4>Março</h4>
+                        </div>
+                        <div class="col-1">
+                            <h4>Abril</h4>
+                        </div>
+                        <div class="col-1">
+                            <h4>Maio</h4>
+                        </div>
+                        <div class="col-1">
+                            <h4>Junho</h4>
+                        </div>
+                        <div class="col-1">
+                            <h4>Julho</h4>
+                        </div>
+                        <div class="col-1">
+                            <h4>Agosto</h4>
+                        </div>
+                        <div class="col-1">
+                            <h4>Setembro</h4>
+                        </div>
+                        <div class="col-1">
+                            <h4>Outubro</h4>
+                        </div>
+                        <div class="col-1">
+                            <h4>Novembro</h4>
+                        </div>
+                        <div class="col-1">
+                            <h4>Dezembro</h4>
+                        </div>
+                    </div>
+                </div>
+                <!--Box 3-->
+                <div class="container dashboard-box3 mt-3">
+                    <div class=" row dashboard3">
+                        <div class="col-2 d-inline-block">
+                            <h6>Brasil</h6>
+
+                        </div>
+                        <div class="col-2 d-inline-block">
+                            <h6>Chile</h6>
+                        </div>
+                        <div class="col-2 d-inline-block">
+                            <h6>Colômbia</h6>
+                        </div>
+                        <div class="col-2 d-inline-block">
+                            <h6>Ecuador</h6>
+                        </div>
+                        <div class="col-2 d-inline-block">
+                            <h6>Central America</h6>
+                        </div>
+                        <div class="col-2 d-inline-block">
+                            <h6>Mexico</h6>
+                        </div>
+
+                    </div>
+                </div>
+                <!--Box 4-->
+                <div class="container dashboard-box4 mt-3">
+                    <div class=" row dashboard4 pt-3">
+                        <div class="col-3 d-inline-block">
+                            <div class="pG-box col-12">
+                                <div style="background-color: #881798; color:#881798;">
+                                    P&G - Neurobion
+                                </div>
+                                <div style="background-color: #f6f6f6; height:100%">
+                                    P&G -NEUROBION
+                                    <p style="padding: 0px 10px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit adipisci, quia molestiae optio, excepturi, eos assumenda aperiam ducimus at magni voluptate voluptatibus nesciunt eius exercitationem fuga a debitis? Inventore, modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus magnam, voluptatem asperiores alias quas cum dicta itaque reprehenderit ipsum eius amet</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3 d-inline-block">
+                            <div class="pg-box2 col-12">
+                                <div style="background-color: #4f6bed; color:#4f6bed;">
+                                    P&G - CONTROLE
+                                </div>
+                                <div style="background-color: #f6f6f6; height:100%">P&G - CONTROLE
+                                    <p style="padding: 0px 10px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit adipisci, quia molestiae optio, excepturi, eos assumenda aperiam ducimus at magni voluptate voluptatibus nesciunt eius exercitationem fuga a debitis? Inventore, modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus magnam, voluptatem asperiores alias quas cum dicta itaque reprehenderit ipsum eius amet</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3 d-inline-block">
+                            <div class="takeda-box col-12">
+                                <div style="background-color: #69797e; color:#69797e;">
+                                    TAKEDA
+                                </div>
+                                <div style="background-color: #f6f6f6; height:100%">TAKEDA
+                                    <p style="padding: 0px 10px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit adipisci, quia molestiae optio, excepturi, eos assumenda aperiam ducimus at magni voluptate voluptatibus nesciunt eius exercitationem fuga a debitis? Inventore, modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus magnam, voluptatem asperiores alias quas cum dicta itaque reprehenderit ipsum eius amet</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3 d-inline-block">
+                            <div class="clube-naos-box col-12">
+                                <div style="background-color: #498205; color:#498205;">
+                                    CLUBE NAOS
+                                </div>
+                                <div style="background-color: #f6f6f6; height:100%">CLUBE NAOS
+                                    <p style="padding: 0px 10px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit adipisci, quia molestiae optio, excepturi, eos assumenda aperiam ducimus at magni voluptate voluptatibus nesciunt eius exercitationem fuga a debitis? Inventore, modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus magnam, voluptatem asperiores alias quas cum dicta itaque reprehenderit ipsum eius amet unde </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
